30cbac096718cc14b9a4b90cc04e0fdd700346cf
mihai10001 <rvivits@gmail.com>
2020-12-10 00:12:07 +0200

    serve first static HTML page

--- app/routes/routes.js
+++ app/routes/routes.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 module.exports = function(app, db) {
-      app.get('/', (req, res) => { 
-        res.send('Hello world :P');
+      app.get('/', (req, res) => {
+        // res.send('Hello world :P');
+        res.sendFile('views/index.html', {root: path.join(__dirname, '..') })
     });
 };
