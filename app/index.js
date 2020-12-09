973df295209a648600bba3f7241ac956e359e13d
mihai10001 <rvivits@gmail.com>
2020-12-10 00:10:10 +0200

    initial routes.js

--- app/index.js
+++ app/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const routes = require('./routes');
 // const MongoClient = require('mongodb').MongoClient;
 
 const app = express();
 const port = 8000;
 
 // Routes/endpoints go here
-// require('./routes')(app, {});
+routes(app, {});
 
 app.listen(port, () => {  
     console.log('Hello world from ' + port);
